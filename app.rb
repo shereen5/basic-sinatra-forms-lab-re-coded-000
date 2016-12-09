9707ff4e56ce7dd57c5a937317e77352fe809080
shereen messi <shereenmessi@hotmail.com>
2016-12-09 09:33:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+get '/newteam' do
+erb :newteam
+end
 
+post '/team' do
+  @team_name=params[:name]
+  @coach=params[:coach]
+  @sg=params[:sg]
+  @pg=params[:pg]
+  @sf=params[:sf]
+  @pf=params[:pf]
+  @c=params[:c]
+  erb :team
+end
 
 end
